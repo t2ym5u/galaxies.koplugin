61c03dd92452d3f5fbec3852ed70bf81ea7c6e90
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:35 +0200

    i18n: add missing FR translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,3 +1,6 @@
 return {
     ["%1\xC3\x97%2 \xC2\xB7 %3 galaxies \xC2\xB7 Unassigned: %4"] = { fr = "%1×%2 · %3 galaxies · Non assignées : %4" },
+    ["Congratulations! All galaxies complete!"] = { fr = "Félicitations ! Toutes les galaxies sont complètes !" },
+    ["Divide the grid into rotationally symmetric galaxies"] = { fr = "Divisez la grille en galaxies à symétrie de rotation" },
+    ["Galaxies"] = { fr = "Galaxies" },
 }
