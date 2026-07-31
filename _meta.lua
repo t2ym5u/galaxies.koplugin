ed82ecc32b6553da940a0e7d913e3c9e1838d1e5
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-31 17:48:55 +0200

    chore: bump version to 1.1.10

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Galaxies"),
     description = _("Divide the grid into rotationally symmetric galaxies"),
-    version     = "1.1.9",
+    version     = "1.1.10",
 }
